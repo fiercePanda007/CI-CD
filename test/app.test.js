c31d24f197e7dc6267b059d0f0a2e4f01a36f84e
Hasib <h.hossain7864@gmail.com>
2025-02-02 12:18:41 +0600

    New

--- test/app.test.js
+++ test/app.test.js
@@ -1,15 +1,15 @@
-const request = require('supertest');
-const app = require('../index');
+const request = require("supertest");
+const app = require("../index");
 
-describe('GET /', () => {
-  it('should return Hello World2!', async () => {
-    const res = await request(app).get('/');
+describe("GET /", () => {
+  it("should return Hello World2!", async () => {
+    const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello World2!');
+    expect(res.text).toBe("Hello World!");
   });
 
-//   it('should fail if the response is not Hello World!', async () => {
-//     const res = await request(app).get('/');
-//     expect(res.text).toBe('Hello Universe!'); // Intentional failing test
-//   });
+  //   it('should fail if the response is not Hello World!', async () => {
+  //     const res = await request(app).get('/');
+  //     expect(res.text).toBe('Hello Universe!'); // Intentional failing test
+  //   });
 });
